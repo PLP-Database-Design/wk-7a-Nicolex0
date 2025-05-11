4af5e14d7fab77174f305e4a7bdef0ce4dec3aae
Nicole Apono <napononicole@gmail.com>
2025-05-12 00:05:25 +0300

    Creatinga a new table for 1NF and inserting data onto the table by splitting the products column

---
+++ answers.sql
@@ -0,0 +1,17 @@
+-- Question 1 - Achieving First Normal Form
+-- Create a new table for 1NF
+CREATE TABLE ProductDetail_1NF (
+    OrderID INT,
+    CustomerName VARCHAR(50),
+    Product VARCHAR(50)
+);
+
+-- Inserting data into the 1NF table by splitting Products column
+INSERT INTO ProductDetail_1NF (OrderID, CustomerName, Product)
+VALUES
+    (101, 'John Doe', 'Laptop'),
+    (101, 'John Doe', 'Mouse'),
+    (102, 'Jane Smith', 'Tablet'),
+    (102, 'Jane Smith', 'Keyboard'),
+    (102, 'Jane Smith', 'Mouse'),
+    (103, 'Emily Clark', 'Phone');
